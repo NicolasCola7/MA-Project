69bad704387c49cf94905afbfa8e72a8327fddd9
alessandrocampedelli <85106363+alessandrocampedelli@users.noreply.github.com>
2025-08-06 14:58:58 +0200

    fix libreria Places

--- app/src/main/java/com/example/travel_companion/BaseApplication.kt
+++ app/src/main/java/com/example/travel_companion/BaseApplication.kt
@@ -1,14 +1,23 @@
 package com.example.travel_companion
 
 import android.app.Application
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.net.PlacesClient
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 @HiltAndroidApp
 class BaseApplication: Application() {
+    lateinit var placesClient: PlacesClient
 
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+
+        if (!Places.isInitialized()) {
+            Places.initialize(applicationContext, "AIzaSyDEVW0HX64ZlwkoVMAZVr7OqgKO4IAuWno")
+        }
+
+        placesClient = Places.createClient(this)
     }
 }
